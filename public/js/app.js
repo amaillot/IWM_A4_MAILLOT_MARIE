d0807e6ddf9098b8d3d831a955ebbfac3acf8488
Luc MARIE <lucmarie.lm@gmail.com>
2017-10-13 11:59:51 +0200

    avancement module borne

--- public/js/app.js
+++ public/js/app.js
@@ -780,40 +780,28 @@ module.exports = __webpack_require__(40);
 /* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
 __webpack_require__(10);
-
 window.Vue = __webpack_require__(35);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
 Vue.component('example', __webpack_require__(36));
-
 var app = new Vue({
     el: '#app'
 });
-
 $(document).ready(function () {
-
     $(".button").click(function (e) {
         e.preventDefault();
-
         /*var env = $("input[name=env]").val();*/
-
         var env = "it works";
-
         var TheId = $(this).attr('id');
-
         $.ajax({
             method: 'GET',
             url: '/items/select',
@@ -823,16 +811,30 @@ $(document).ready(function () {
                 id: TheId
             },
             success: function success(data) {
-                console.log(data.success);
+                //                console.log(data.success);
                 var nouvelItem = data.success.nouvelItem;
                 var total = data.success.prix;
-                $(".panel-selected").append("<div><h3>" + nouvelItem.Nom + "</h3><p style='float: right;'>Prix : " + nouvelItem.Prix + "</p><button class='remove'>Remove</button></div>");
+                $(".panel-selected ").append("<div class='item_list_product'><h3>" + nouvelItem.Nom + "</h3><div style='display:inline-block;float: right;'><div class='list_prix'>Prix :</div> <div class='prix_val'>" + nouvelItem.Prix + "</div>€</div><button class='remove'>Remove</button></div>");
+                $('.no_item').hide();
+                var total = 0;
+                $('.prix_val').each(function () {
+                    total += parseFloat($(this).html());
+                    $('.panel-footer .total').html(total + '€'); // Or this.innerHTML, this.innerText
+                });
             }
         });
     });
-
     $("body").on('click', '.remove', function () {
         $(this).parent().remove();
+        var total = 0;
+        $('.prix_val').each(function () {
+            total += parseFloat($(this).html());
+            $('.panel-footer .total').html(total + '€');
+        });
+        if (total == 0) {
+            $('.panel-footer .total').html(total + '€');
+            $('.no_item').show();
+        }
     });
 });
 
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,58 +1,58 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
 require('./bootstrap');
-
 window.Vue = require('vue');
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
 Vue.component('example', require('./components/Example.vue'));
-
 const app = new Vue({
     el: '#app'
 });
-
-$( document ).ready(function() {
-
-    $(".button").click(function(e){
+$(document).ready(function () {
+    $(".button").click(function (e) {
         e.preventDefault();
-
         /*var env = $("input[name=env]").val();*/
-
         var env = "it works";
-
         var TheId = $(this).attr('id');
-
         $.ajax({
-            method:'GET',
-            url:'/items/select',
-            data:{
-                env:env,
-                _token:"{{ csrf_token() }}",
-                id : TheId
-            },
-            success:function(data){
-                console.log(data.success);
+            method: 'GET'
+            , url: '/items/select'
+            , data: {
+                env: env
+                , _token: "{{ csrf_token() }}"
+                , id: TheId
+            }
+            , success: function (data) {
+                //                console.log(data.success);
                 var nouvelItem = data.success.nouvelItem;
                 var total = data.success.prix;
-                $(".panel-selected").append(
-                    "<div><h3>"+nouvelItem.Nom+"</h3><p style='float: right;'>Prix : "+nouvelItem.Prix+"</p><button class='remove'>Remove</button></div>"
-                );
+                $(".panel-selected ").append("<div class='item_list_product'><h3>" + nouvelItem.Nom + "</h3><div style='display:inline-block;float: right;'><div class='list_prix'>Prix :</div> <div class='prix_val'>" + nouvelItem.Prix + "</div>€</div><button class='remove'>Remove</button></div>");
+                $('.no_item').hide();
+                var total = 0;
+                $('.prix_val').each(function () {
+                    total += parseFloat($(this).html());
+                    $('.panel-footer .total').html(total + '€'); // Or this.innerHTML, this.innerText
+                });
             }
         });
     });
-
-    $("body").on('click', '.remove', function ()
-    {
+    $("body").on('click', '.remove', function () {
         $(this).parent().remove();
+        var total = 0;
+        $('.prix_val').each(function () {
+            total += parseFloat($(this).html());
+            $('.panel-footer .total').html(total + '€');
+ 
+        });
+        if (total == 0) {
+            $('.panel-footer .total').html(total + '€');
+            $('.no_item').show();
+        }
     });
 });
